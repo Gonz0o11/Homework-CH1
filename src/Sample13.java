b7b4beb37245f0c3577c4ea428c7696fbae2d2aa
Gonz0o11 <Gonz0o101010@gmail.com>
2024-09-28 17:55:46 +0800

    first

---
+++ src/Sample13.java
@@ -0,0 +1,13 @@
+public class Sample13 {
+    public static void main(String[] args) {
+        double dnum = 160.5;
+
+        System.out.println("身高是" + dnum + "公分");
+
+        System.out.println("指定給int型態的變數");
+
+        int num = (int) dnum;
+
+        System.out.println("身高是" + num + "公分");
+    }
+}
